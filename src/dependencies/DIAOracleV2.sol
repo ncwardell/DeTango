6ea782c97ba0690b0ea13988641ae0a5a6d22bf1
Noah <41527027+ncwardell@users.noreply.github.com>
2025-12-02 19:42:54 -0500

    Delete src directory

--- src/dependencies/DIAOracleV2.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.4;
-
-interface DIAOracleV2 {
-
-    function getValue(string memory key) external view returns (uint128 value, uint128 timestamp);
-
-}
--- src/dependencies/IPauseable.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: unlicenced
-pragma solidity ^0.8.4;
-
-interface IPauseable {
-
-    function paused() external view returns (bool);
-
-}
